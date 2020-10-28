3e91d83e3a2573bbe8185e6dfc51105288ec8e12
Виктор Федоров <fdorov@mail.ru>
2020-10-28 11:38:39 +0300

    испправлена ошибка в определении функции sitedit! модуля HorizonSideRobots

--- src/HorizonSideRobots.jl
+++ src/HorizonSideRobots.jl
@@ -222,7 +222,7 @@ sitcreate(num_rows::Integer,num_colons::Integer; newfile="untitled.sit") = sited
 
 -- позволяет транслировать в уже имеющийся объект типа Robot обстановку из файла
 """
-sitedit!(r::Robot,sitfile::AbstractString) = sitedit!(r.situation, sitfile)
+SituationData.sitedit!(r::Robot,sitfile::AbstractString) = sitedit!(r.situation, sitfile)
 
 # вспомогательные функции:
 is_inside(r::Robot) = SituationDatas.is_inside(r.situation) # - проверяет, находится ли Робот в фрейме (в наблюдаемой части поля)
