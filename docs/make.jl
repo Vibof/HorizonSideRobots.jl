b0cab8b2453c02311d48276c8e9eae4ef110dd45
Viktor <fdorov@mail.ru>
2026-08-31 09:34:30 +0300

    Оторедактирован README.md и /docs/src/index.md

--- docs/make.jl
+++ docs/make.jl
@@ -6,23 +6,24 @@ makedocs(;
     authors="Виктор Федоров <fdorov@mail.ru>",
     repo="https://github.com/Vibof/HorizonSideRobots.jl/blob/{commit}{path}#L{line}",
     sitename="HorizonSideRobots.jl",
+    
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://vibof.github.io/HorizonSideRobots.jl/",
         assets=String[],
     ),
 
+
     pages=[
         "Заглавная" => "index.md",
         "Установка и начало работы" => "setup.md",
         "Пример разработки программного кода" => "source_mark_kross.md",
 
-        "Методические материалы" => [
-            "Обзор" => "methodology/index.md",
+        "Курс по основам программирования" => [
+            "Оглавление" => "methodology/index.md",
         ],
 
         "API" => "api.md",
-        "Формат sit-файла" => "sit_format.md",
     ],
 )
 
